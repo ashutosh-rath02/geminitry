d975f0233d196ace09144baae3a80dd4aa4ad466
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-07-03 20:56:17 +0530

    fix: complete chatbox

--- app/components/RenderMessages.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const RenderMessages = () => {
-  return <div>Hey</div>;
-};
-
-export default RenderMessages;
--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,24 @@
-// "use client";
-// import RenderForm from "./components/RenderForm";
-// import RenderMessages from "./components/RenderMessages";
-// import { useChat } from "ai/react";
-
-// export default function Home() {
-//   const { messages } = useChat({
-//     api: "api/genai",
-//   });
-//   return (
-//     <main className="flex min-h-screen flex-col items-center p-12">
-//       <RenderForm />
-//       <RenderMessages />
-//       <div className="text-black">
-//         {" "}
-//         {messages.map((message) => (
-//           <div key={message.id}>
-//             {message.role === "user" ? "User: " : "AI: "}
-//             {message.content}
-//           </div>
-//         ))}
-//       </div>
-//     </main>
-//   );
-// }
 "use client";
-
 import { useChat } from "ai/react";
+import Messages from "./components/messages";
+import InputForm from "./components/inputForm";
 
-export default function Page() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat({
-    api: "api/chat",
-  });
+export default function Home() {
+  const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
+    useChat({
+      api: "api/genai",
+    });
 
   return (
-    <>
-      {messages.map((message) => (
-        <div key={message.id}>
-          {message.role === "user" ? "User: " : "AI: "}
-          {message.content}
-        </div>
-      ))}
-      <form onSubmit={handleSubmit}>
-        <input
-          name="prompt"
-          value={input}
-          onChange={handleInputChange}
-          id="input"
-        />
-        <button type="submit">Submit</button>
-      </form>
-    </>
+    <main className="flex min-h-screen flex-col items-center p-12 text-lg">
+      <InputForm
+        input={input}
+        handleInputChange={handleInputChange}
+        handleSubmit={handleSubmit}
+        isLoading={isLoading}
+        stop={stop}
+      />
+      <Messages messages={messages} isLoading={isLoading} />
+    </main>
   );
 }
