fd1c7d0ccef9e5d4620d968b1c302cf5147dc6ee
ashutosh-rath02 <ashutosh123rath@gmail.com>
2024-07-03 17:19:17 +0530

    feat: add render form

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import RenderForm from "./components/RenderForm";
+import RenderMessages from "./components/RenderMessages";
+import { useChat } from "ai/react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-12">
-      Hello world
+      <RenderForm />
+      <RenderMessages />
     </main>
   );
 }
